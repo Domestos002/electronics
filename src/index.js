72c1da10376027599e0ddfcbc12ce6a424d1edfc
Domestos002 <absorber000@gmail.com>
2017-10-07 06:38:09 +0600

    jsxxxx

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import App from './App';
+import Goods from './containers/Goods'
+import Layout from './containers/Layout'
 import registerServiceWorker from './registerServiceWorker';
 import {createStore, applyMiddleware} from 'redux'
 import reducers from './reducers/Reducers'
+import {browserHistory} from 'react-router'
+import {Router, Route} from 'react-router'
 import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import {syncHistoryWithStore} from 'react-router-redux'
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
 ));
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path="/" component={Goods} />
+            </Route>
+        </Router>
+    </Provider>,
     document.getElementById("root")
+
 );
 registerServiceWorker();
--- src/reducers/Reducers.js
+++ src/reducers/Reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
+import {routerReducer} from 'react-router-redux'
 import Goods from './Goods'
 
 export default combineReducers({
+    routing: routerReducer,
     Goods
 })
